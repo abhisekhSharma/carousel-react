e13dce53c04f5a7a5b6c66a7335e0400e2976692
abhishek <asharma102@sapient.com>
2018-08-28 17:02:01 +0100

    middle element almost

--- app/components/SliderCarousel.js
+++ app/components/SliderCarousel.js
@@ -12,7 +12,7 @@ class SliderCarousel extends React.Component {
             counter: 1,
             containerHeight: '0',
             containerWidth: '0',
-            selectedNode:3
+            selectedNode:2
         }
         this.next = this.next.bind(this);
         this.prev = this.prev.bind(this);
@@ -22,9 +22,11 @@ class SliderCarousel extends React.Component {
         this.setState({
             containerHeight: this.ContElement.clientHeight,
             containerWidth: this.ContElement.clientWidth,
-            selectedNode:Math.floor(this.ContElement.clientWidth/this.divElement.firstChild.clientWidth / 2)+1
+            selectedNode:Math.floor(this.ContElement.clientWidth/this.divElement.firstChild.clientWidth / 2)
 
-      }) 
+      })
+
+
     }
     
     componentDidMount() {
@@ -34,35 +36,27 @@ class SliderCarousel extends React.Component {
 
     // Next button event handler
     next() {
-        console.log(this.state.selectedNode);
-        this.setState({
-            counter: this.state.counter+1,
-            
-            selectedNode: this.state.selectedNode > 20 ? 2 : this.state.selectedNode+1
-        });
-        
-        var scroll = this.state.counter
         this.divElement.style.right = this.state.counter*200+"px";
         this.divElement.style.left = 0+"px";
         this.divElement.lastChild.parentNode.insertBefore(this.divElement.firstChild,this.divElement.lastChild.nextSibling);
-        
+        this.divElement.children[this.state.selectedNode].classList.add("selected");
+        this.divElement.children[this.state.selectedNode].previousSibling.classList.remove("selected");
     }
 
     // Next button event handler
     prev() {
-        this.setState({
-            selectedNode: this.state.selectedNode-1
-        });
         this.divElement.lastChild.parentNode.insertBefore(this.divElement.lastChild,this.divElement.firstChild)
         this.divElement.style.left = 0+"px";
-        console.log(this.divElement.firstChild.parentNode);
+        console.log(this.state.selectedNode);
+        this.divElement.children[this.state.selectedNode].classList.add("selected");
+        this.divElement.children[this.state.selectedNode].nextSibling.classList.remove("selected");
     }
     
     render() {
         
     //iterating carousel slide with payload object
     const payload = Object.keys(this.props.payload).map(key => 
-    	<div key={key} id={key} className={ this.state.selectedNode-1 == key ? 'selected slider' : 'slider s' }    ref={ (divSlider) => this.divSlider = divSlider}>
+    	<div key={key} id={key} className="slider"    ref={ (divSlider) => this.divSlider = divSlider}>
             <div className="img-cont"><img src={this.props.payload[key].largeImageURL} alt={this.props.payload[key].tags} /></div>
             <p>img{}{this.props.payload[key].user}{this.state.selectedNode}</p>
         </div>
